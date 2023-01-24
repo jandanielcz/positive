2b55a490508a20659cca6694d2605c47b9da16be
jandanielcz <jandanielcz@gmail.com>
2023-01-24 09:27:59 +0100

    clean container construction

--- public/index.php
+++ public/index.php
@@ -1,45 +1,43 @@
 <?php
 declare(strict_types=1);
 
-use Laminas\Diactoros\ServerRequestFactory;
-use League\Container\Container;
+use Jandanielcz\Positive\ContainerBuilder;
 use League\Route\Router;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 require '../vendor/autoload.php';
 
-/** @var Container $c */
-$c = include '../src/container.php';
+$container = ContainerBuilder::build();
 
-$request = ServerRequestFactory::fromGlobals($_SERVER, $_GET, $_POST, $_FILES);
+$request = $container->get(\Laminas\Diactoros\ServerRequest::class);
 
 /** @var Router $router */
-$router = $c->get(Router::class);
+$router = $container->get(Router::class);
 
 $router->map('GET', '/image/{year}/{path}',
-    function (ServerRequestInterface $request, array $args) use ($c): ResponseInterface  {
-        $controller = $c->get(\Jandanielcz\Positive\ImageController::class);
+    function (ServerRequestInterface $request, array $args) use ($container): ResponseInterface  {
+        $controller = $container->get(\Jandanielcz\Positive\ImageController::class);
         return $controller->get($request, $args['year'], $args['path']);
     });
 $router->map('GET', '/rss2',
-    function (ServerRequestInterface $request) use ($c): ResponseInterface  {
-        $controller = $c->get(\Jandanielcz\Positive\FeedController::class);
+    function (ServerRequestInterface $request) use ($container): ResponseInterface  {
+        $controller = $container->get(\Jandanielcz\Positive\FeedController::class);
         return $controller->rss2($request);
     });
 $router->map('GET', '/atom',
-    function (ServerRequestInterface $request) use ($c): ResponseInterface  {
-        $controller = $c->get(\Jandanielcz\Positive\FeedController::class);
+    function (ServerRequestInterface $request) use ($container): ResponseInterface  {
+        $controller = $container->get(\Jandanielcz\Positive\FeedController::class);
         return $controller->atom($request);
     });
 $router->map('GET', '/',
-    function (ServerRequestInterface $request) use ($c): ResponseInterface  {
-        $controller = $c->get(\Jandanielcz\Positive\IndexController::class);
+    function (ServerRequestInterface $request) use ($container): ResponseInterface  {
+        $controller = $container->get(\Jandanielcz\Positive\IndexController::class);
         return $controller->index($request);
     });
 
 $response = $router->dispatch($request);
 
 /** @var \Laminas\HttpHandlerRunner\Emitter\SapiEmitter $emitter */
-$emitter = $c->get(\Laminas\HttpHandlerRunner\Emitter\SapiStreamEmitter::class);
+$emitter = $container->get(\Laminas\HttpHandlerRunner\Emitter\SapiStreamEmitter::class);
 $emitter->emit($response);
---
+++ src/ContainerBuilder.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Jandanielcz\Positive;
+
+use Laminas\Diactoros\ServerRequest;
+use Laminas\Diactoros\ServerRequestFactory;
+use League\Container\Container;
+use League\Container\ReflectionContainer;
+
+class ContainerBuilder
+{
+    const BASEDIR = __DIR__ . '/../';
+
+    public static function build(): Container
+    {
+        $c = new Container();
+
+        $c->delegate(
+            new ReflectionContainer()
+        );
+
+
+
+        $c->add(\League\Route\Router::class);
+        $c->add(\League\Plates\Engine::class)->addArgument('../templates');
+        $c->add(Configuration::class)
+            ->addArgument([self::BASEDIR .'conf/site.defaults.json5', self::BASEDIR .'conf/site.json5']);
+        $c->add(\League\Glide\Urls\UrlBuilder::class, function() use ($c) {
+            /** @var Configuration $configuration */
+            $configuration = $c->get(Configuration::class);
+            $key = $configuration->get('site::glidekey');
+            $urlBuilder = \League\Glide\Urls\UrlBuilderFactory::create('/image/', $key);
+            return $urlBuilder;
+        });
+        $c->add(\League\Glide\Signatures\Signature::class, function() use ($c) {
+            /** @var Configuration $configuration */
+            $configuration = $c->get(Configuration::class);
+            $key = $configuration->get('site::glidekey');
+            $signature = \League\Glide\Signatures\SignatureFactory::create($key);
+            return $signature;
+        });
+        $c->add(\Jandanielcz\Positive\Posts::class)
+            ->addArgument(self::BASEDIR . 'content/posts.listofjsons')
+            ->addArgument($c->get(\League\Glide\Urls\UrlBuilder::class));
+        $c->add(\League\Glide\Server::class, function() {
+            $server = \League\Glide\ServerFactory::create([
+                'source' =>  self::BASEDIR . 'content/images',
+                'cache' =>  self::BASEDIR . 'content/cache',
+            ]);
+
+            $server->setResponseFactory(
+                new League\Glide\Responses\PsrResponseFactory(
+                    new \Laminas\Diactoros\Response(), function ($stream) {
+                        return new \Laminas\Diactoros\Stream($stream);
+                    }
+                )
+            );
+
+            return $server;
+        });
+
+        $c->add(ServerRequest::class, function() {
+            return ServerRequestFactory::fromGlobals($_SERVER, $_GET, $_POST, $_FILES);
+        });
+
+        return $c;
+    }
+}
